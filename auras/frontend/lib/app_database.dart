40e2d03f2087592a485bd92841e4a24a7b91105b
Ana Beatriz Ramos <anabeatrizramosdosantos@gmail.com>
2024-06-15 02:33:51 -0300

    Atualização

--- auras/frontend/lib/app_database.dart
+++ auras/frontend/lib/app_database.dart
@@ -1,128 +1,6 @@
-import 'package:avc_form/main.dart';
+import 'package:auras/main.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
-
-
-class DiseasePrediction {
-  final int? id;
-  final String name;
-  final String cpf;
-  final String dtnascimento;
-  final double glucose;
-  final double cholesterol;
-  final double hemoglobin;
-  final double platelets;
-  final double white_blood_cells;
-  final double red_blood_cells;
-  final double hematocrit;
-  final double mean_corpuscular_volume;
-  final double mean_corpuscular_hemoglobin;
-  final double mean_corpuscular_hemoglobin_concentration;
-  final double insulin;
-  final double bmi;
-  final double systolic_blood_pressure;
-  final double diastolic_blood_pressure;
-  final double triglycerides;
-  final double hba1c;
-  final double ldl_cholesterol;
-  final double hdl_cholesterol;
-  final double alt;
-  final double ast;
-  final double heart_rate;
-  final double creatinine;
-  final double troponin;
-  final double c_reactive_protein;
-  final int disease;
-
-  DiseasePrediction(
-    this.id,
-    this.name,
-    this.cpf,
-    this.dtnascimento,
-    this.glucose,
-    this.cholesterol,
-    this.hemoglobin,
-    this.platelets,
-    this.white_blood_cells,
-    this.red_blood_cells,
-    this.hematocrit,
-    this.mean_corpuscular_volume,
-    this.mean_corpuscular_hemoglobin,
-    this.mean_corpuscular_hemoglobin_concentration,
-    this.insulin,
-    this.bmi,
-    this.systolic_blood_pressure,
-    this.diastolic_blood_pressure,
-    this.triglycerides,
-    this.hba1c,
-    this.ldl_cholesterol,
-    this.hdl_cholesterol,
-    this.alt,
-    this.ast,
-    this.heart_rate,
-    this.creatinine,
-    this.troponin,
-    this.c_reactive_protein,
-    this.disease,
-  );
-
-  Map<String, dynamic> toMap() {
-    return {
-      'id': id,
-      'name': name,
-      'cpf': cpf,
-      'dtnascimento': dtnascimento,
-      'glucose': glucose,
-      'cholesterol': cholesterol,
-      'hemoglobin': hemoglobin,
-      'platelets': platelets,
-      'white_blood_cells': white_blood_cells,
-      'red_blood_cells': red_blood_cells,
-      'hematocrit': hematocrit,
-      'mean_corpuscular_volume': mean_corpuscular_volume,
-      'mean_corpuscular_hemoglobin': mean_corpuscular_hemoglobin,
-      'mean_corpuscular_hemoglobin_concentration': mean_corpuscular_hemoglobin_concentration,
-      'insulin': insulin,
-      'bmi': bmi,
-      'systolic_blood_pressure': systolic_blood_pressure,
-      'diastolic_blood_pressure': diastolic_blood_pressure,
-      'triglycerides': triglycerides,
-      'hba1c': hba1c,
-      'ldl_cholesterol': ldl_cholesterol,
-      'hdl_cholesterol': hdl_cholesterol,
-      'alt': alt,
-      'ast': ast,
-      'heart_rate': heart_rate,
-      'creatinine': creatinine,
-      'troponin': troponin,
-      'c_reactive_protein': c_reactive_protein,
-      'disease': disease,
-    };
-  }
-}
-
-class Usuario {
-  final int id;
-  final String user;
-  final String cpfUser;
-  final String email;
-  final String funcao;
-  final String password;
-  final String passwordChecks;
-
-  Usuario(
-    this.id,
-    this.user,
-    this.cpfUser,
-    this.email,
-    this.funcao,
-    this.password,
-    this.passwordChecks,
-  );
-}
-
-
-
 
 // CREATE DATABASE
 Future<Database> createDatabase() {
@@ -250,7 +128,6 @@ Future<List<DiseasePrediction>> findAllDisease() {
                 map['creatinine'],
                 map['troponin'],
                 map['c_reactive_protein'],
-                map['disease'],
             );
             diseasepredictions.add(diseaseprediction);
           }
@@ -260,31 +137,3 @@ Future<List<DiseasePrediction>> findAllDisease() {
     },
   );
 }
-
-// GET ID
-Future<Usuario?> findByIdUser(String email) {
-  return createDatabase().then(
-    (db) {
-      return db.query('usuarios', where: 'email = ?', whereArgs: [email]).then(
-        (maps) {
-          if (maps.isNotEmpty) {
-            final Map<String, dynamic> map = maps.first;
-            final Usuario usuario = Usuario(
-              map['id'],
-              map['user'],
-              map['cpfUser'],
-              map['email'],
-              map['funcao'],
-              map['password'],
-              map['passwordChecks'],
-            );
-            return usuario;
-          } else {
-            return null; // Retorna null se não encontrar nenhum usuário com o ID fornecido
-          }
-        },
-      );
-    },
-  );
-}
-
